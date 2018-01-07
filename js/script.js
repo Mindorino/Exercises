fa8e95fff0a93acde60fb0b3235885e3a64a4309
Tomasz nowakowski <serafin94@tlen.pl>
2018-01-07 19:52:03 +0100

    Copying existing files to respository /Getiing started online

---
+++ js/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var imiona = ['Marcin', 'Ania', 'Agnieszka', 'Andrzej', 'Roman', 'Dawid'];
+//imiona.sort();
+//console.log(imiona);
+//
+//var marcin = {
+//
+//wiek: 24,
+//wzrost: "174cm",
+//"kolor oczu": "brąz",
+//metoda: "próbka wysoce wątpliwa i odbiega od normy"
+//};
+//
+//var tomasz = {
+//    wiek: 23,
+//    ambitny: true,
+//    auta: ["cougarFord", "maluchfiat", "renaultMegane"],
+//    metoda: "fight the future"
+//};
+//
+//var roman = {
+//    wiek:24,
+//    "kolor oczu": "takise"
+//}
+
+
+//var imie = "olek";
+//console.log("Cześć " + imie );
+//if (imie.length > 7) {
+//    console.log("Wow, masz na prawdę długie imie")
+//}
+//else {
+//    console.log("twoje imie w sumie nie jest takie długie")
+//}
+//;
+
+//for (var policzoneOwce = 0; policzoneOwce <= 10; policzoneOwce++) {
+//    console.log("Owca numer " + policzoneOwce + " już policzona")
+//}
+//console.log("Zasnąwszy :D");
+
+
+//for (var i = 0; i < imiona.length; i++) {
+//    console.log("Cześć! " + imiona[i] + ".");
+//}
+
+//var mojaGodnosc = "Tomasz Jan Serafin Nowakowski"
+//for (var i = 0; i < mojaGodnosc.length; i++) {
+//    console.log("litera numer" + [i] + "jest literą " + mojaGodnosc[i])
+//}
+//console.log("to tyle");
+
+//for (var i = 0; i < imiona.length; i++) {
+//    if (imiona[i].length < 6) { 
+//    imiona[i] = "Zajebisty " + imiona[i];
+//    console.log(imiona[i]);
+//    }
+//    else {
+//        imiona[i] = "Chujowy " + imiona[i];
+//        console.log(imiona[i]);
+//    }
+//}
+
+//var alfabet = "qwertyuiopasdfghjklzxcvbnm";
+//var lancuch = "";
+//while (lancuch.length < 10000) {
+//    lancuch += alfabet[Math.floor(Math.random() * alfabet.length)];
+//    
+//    
+//    
+//}
+//
+//console.log(lancuch);
+
+//var lancuchWejsciowy = "javascript jest superowe";
+//var lancuchWyjsciowy = "";
+//
+//for (var i = 0; i < lancuchWejsciowy.length; i++) {
+//    if (lancuchWejsciowy[i] === "a") {
+//        lancuchWyjsciowy += "4";
+//    } else if (lancuchWejsciowy[i] === "i") {
+//        lancuchWyjsciowy += "1";
+//    } else if (lancuchWejsciowy[i] === "o") {
+//        lancuchWyjsciowy += "0";
+//    } else {
+//        lancuchWyjsciowy += lancuchWejsciowy[i] 
+//    }
+//}
+//console.log(lancuchWyjsciowy);
+
+
---
+++ js/theHangMan.js
@@ -0,0 +1,5 @@
+'use strict';
+
+
+var imie = prompt("Jak masz na imie?");
+console.log("Cześć " + imie);
