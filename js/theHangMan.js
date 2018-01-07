b0f0dbe826ce2dfbbf0fd665d61a3a8075ef10b1
Tomasz nowakowski <serafin94@tlen.pl>
2018-01-07 23:36:14 +0100

    First section of the hang-man Game

--- js/theHangMan.js
+++ js/theHangMan.js
@@ -1,5 +1,11 @@
 'use strict';
 
 
-var imie = prompt("Jak masz na imie?");
-console.log("Cześć " + imie);
+var slowa = ["Zajebisty", "Cougar", "Tomasz", "Maluch", "Safrane"];
+var slowo = slowa[Math.floor(Math.random() * slowa.length)];
+var odpowiedzi = [];
+for (var i = 0;i < slowo.length;i++) {
+    odpowiedzi[i] = "_";
+}
+var pozostaleLitery = slowo.length;
+alert(odpowiedzi.join(" "));
