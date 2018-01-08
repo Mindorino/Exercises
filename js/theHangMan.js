4447e355141a7f193e0cc8d41272f9cd37a6bad8
Tomasz nowakowski <serafin94@tlen.pl>
2018-01-08 02:11:55 +0100

    TheHangMan Game has been succesfully written :D

--- js/theHangMan.js
+++ js/theHangMan.js
@@ -1,11 +1,42 @@
 'use strict';
 
 
-var slowa = ["Zajebisty", "Cougar", "Tomasz", "Maluch", "Safrane"];
+var slowa = ["zajebisty", "cougar", "tomasz", "maluch", "safrane"];
 var slowo = slowa[Math.floor(Math.random() * slowa.length)];
 var odpowiedzi = [];
-for (var i = 0;i < slowo.length;i++) {
+for (var i = 0; i < slowo.length; i++) {
     odpowiedzi[i] = "_";
 }
 var pozostaleLitery = slowo.length;
+var strzaly = 20;
+
+while (pozostaleLitery > 0 && strzaly > 0) {
+    alert(odpowiedzi.join(" "));
+
+    var strzal = prompt("Podaj Literę, lub klinij anuluj aby zakończyć.");
+    if (strzal === null) {
+        break;
+    } else if (strzal.length !== 1) {
+        alert("Proszę podaj tylko jedną literę.");
+        
+    } else {
+        strzal = strzal.toLowerCase();
+        strzaly--;
+        
+        for (var j = 0; j < slowo.length; j++) {
+            if (slowo[j] === strzal && odpowiedzi[j] === "_") {
+                odpowiedzi[j] = strzal;
+                pozostaleLitery--;
+            }
+        }
+    }
+}
 alert(odpowiedzi.join(" "));
+if (strzaly > 0 && pozostaleLitery === 0) {
+   alert("Gratulacje! Szukane słowo to " + slowo); 
+}
+else {
+    alert("No niestety. Słowem tym było " + slowo);
+}
+
+
